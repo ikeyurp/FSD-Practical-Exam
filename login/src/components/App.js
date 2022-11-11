f73a987fce790f6c0cb66aca58e564c0a8b7fc10
Keyur Patel <keyurbhut12345@gmail.com>
2022-11-11 05:07:25 +0000

    First

---
+++ login/src/components/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import NewComponent from './login/login';
+
+function App() {
+  return (
+    <div className="App">
+      <NewComponent />
+    </div>
+  );
+}
+
+export default App;
